6af002d55207036b46e3ee65e5fa962ba540865e
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2022-08-07 13:20:32 +0200

    Playing around with SQL

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,38 @@
+import org.apache.spark.sql.SparkSession
+
 object Main {
-  
+
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("Anime_Analysis")
+      .master("local[*]")
+      .getOrCreate()
+
+    val animeDF = spark.read
+      .option("header", "true")
+      .schema(Schemas.animeSchema)
+      .format("csv")
+      .load("data/anime.csv")
+
+    val ratingDF = spark.read
+      .option("header", "true")
+      .schema(Schemas.ratingSchema)
+      .format("csv")
+      .load("data/rating.csv")
+
+    // animeDF.show(10)
+    // ratingDF.show(10)
+
+//    animeDF.join(ratingDF, animeDF("anime_id") === ratingDF("anime_id"), "inner" )
+//      .show(false)
+
+    animeDF.createTempView("ANIME")
+    ratingDF.createTempView("RATING")
+
+    // top 10 best rated animes of all time
+    val topRatedAnimeDF = spark.sql("SELECT name, rating FROM ANIME WHERE rating > 9 AND type = 'TV' ")
+    topRatedAnimeDF.show(10)
+  }
 }
